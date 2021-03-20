825fc87824f7f965c430dbb5869968f491194123
tfrederick1 <60411410+tfrederick1@users.noreply.github.com>
2021-03-20 16:37:29 -0400

    Google Calendar
    Implemented google calendar, made even more stylesheet changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
 import About_Us from './components/About_Us/About_Us';
 import Board_Members from './components/About_Us/Board_Members';
@@ -7,83 +8,118 @@ import Sponsors from './components/About_Us/Sponsors';
 import Contact_Us from './components/About_Us/Contact_Us';
 import Home from './components/Home/Home';
 import Outreach from './components/Outreach/Outreach';
+import Calendar from './components/Calendar/Calendar';
 
-function App() {
-    return (
-        <div className="wrapper">
-            <div class="flex-container-header">
-                <div class="wrap">
-                    <h1> Gators Society of Undergraduate Surgeons </h1>
-                    <h2> -Add text about mission statement. The rest of this is just filler to test what happens when it needs to wrap.-</h2>
-                </div>
-            </div>
-
-            <div class="flex-container-bar">
-                <div class="flex-container-helper">
-                    <div class="dropdown">
-                        <button class="drop-button">
-                            <a href="/">Home</a>
-                        </button>
+class App extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <div class="flex-container-header">
+                    <div class="wrap">
+                        <h1> Gators Society of Undergraduate Surgeons </h1>
+                        <h2> -Add text about mission statement. The rest of this is just filler to test what happens when it needs to wrap.-</h2>
                     </div>
-                    <div class="dropdown">
-                        <button class="drop-button">
-                            <a href="/About-Us">About Us</a>
-                        </button>
-                        <div class="dropdown-content">
-                            <a href="/About-Us/Board-Members">Board Members</a>
-                            <a href="/About-Us/Sponsors">Sponsors</a>
-                            <a href="/About-Us/Contact-Us">Contact Us</a>
+                </div>
+
+                <div class="flex-container-bar">
+                    <div class="flex-container-helper">
+                        <div class="dropdown">
+                            <button class="drop-button">
+                                <a href="/">Home</a>
+                            </button>
+                        </div>
+                        <div class="dropdown">
+                            <button class="drop-button">
+                                <a href="/About-Us">About Us</a>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="/About-Us/Board-Members">Board Members</a>
+                                <a href="/About-Us/Sponsors">Sponsors</a>
+                                <a href="/About-Us/Contact-Us">Contact Us</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <button class="drop-button">
+                                <a href="/Outreach">Outreach</a>
+                            </button>
+                            <div class="dropdown-content">
+                                <a href="#">Placeholder 1</a>
+                                <a href="#">Placeholder 2</a>
+                                <a href="#">Placeholder 3</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <button class="drop-button">
+                                <a href="/Calendar">Calendar</a>
+                            </button>
                         </div>
                     </div>
-                    <div class="dropdown">
-                        <button class="drop-button">
-                            <a href="/Outreach">Outreach</a>
-                        </button>
-                        <div class="dropdown-content">
-                            <a href="#">Placeholder 1</a>
-                            <a href="#">Placeholder 2</a>
-                            <a href="#">Placeholder 3</a>
-                        </div>
-                    </div>
-                    <div class="dropdown">
-                        <button class="drop-button">
-                            <a href="/Calendar">Calendar</a>
-                        </button>
-                    </div>
                 </div>
-            </div>
 
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/About-Us/Board-Members">
-                        <Board_Members />
-                    </Route>
-                    <Route path='/About-Us/Sponsors'>
-                        <Sponsors />
-                    </Route>
-                    <Route path='/About-Us/Contact-Us'>
-                        <Contact_Us />
-                    </Route>
-                    <Route path="/About-Us">
-                        <About_Us />
-                    </Route>
-                    <Route path="/Outreach">
-                        <Outreach />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-
-            <div class='flex-container-event'>
-                <div>
-                    <h1>Upcoming Events</h1>
-                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
-                </div>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/About-Us/Board-Members">
+                            <Board_Members />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                        <Route path='/About-Us/Sponsors'>
+                            <Sponsors />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                        <Route path='/About-Us/Contact-Us'>
+                            <Contact_Us />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                        <Route path="/About-Us">
+                            <About_Us />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                        <Route path="/Outreach">
+                            <Outreach />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                        <Route path='/Calendar'>
+                            <Calendar />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                            <div class='flex-container-event'>
+                                <div>
+                                    <h1>Upcoming Events</h1>
+                                    <p>This is just a test to figure out what this looks like. Figure out how to pull information from google calendar.</p>
+                                </div>
+                            </div>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Outreach extends React.Component {
+    render() {
+        return (
+            <div>
+                <div class="transbox">
+                    <h1>Calendar</h1>
+                </div>
+                <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=c3Vmc21hbmFnbWVudEBnbWFpbC5jb20&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%230B8043&amp;showTitle=1&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;title=Society%20of%20Undergraduate%20Surgeons%20Event%20Calendar" title="Event Calendar"></iframe>
+            </div>
+        );
+    }
+}
