1ff76ad58245a0b4af860471df7642839fb14284
chasonpratt <58475164+chasonpratt@users.noreply.github.com>
2021-04-13 03:14:57 -0400

    Create Calendar.js

---
+++ src/pages/Calendar/Calendar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GOOGLE_API_KEY } from "../../config.js";
+import Calendar from "@ericz1803/react-google-calendar";
+
+let calendars = [
+  {
+    calendarId: "n3vtus5t2akehqaupb8n4aa2ts@group.calendar.google.com"
+  }
+];
+
+export default class gCalendar extends React.Component {
+  render() {
+    return (
+        <div>
+            <div class="transbox">
+                <h1>Event Calendar</h1>
+            </div>
+            <div class="flex-container-body">
+              <div>
+              <Calendar apiKey={GOOGLE_API_KEY} calendars={calendars} />
+              </div>
+            </div>
+        </div>
+    );
+  }
+} 
