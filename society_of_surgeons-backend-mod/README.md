140c75c607a490000413485bcf854372abe77b08
chasonpratt <58475164+chasonpratt@users.noreply.github.com>
2021-04-19 18:17:39 -0400

    Delete society_of_surgeons-backend-mod directory

--- society_of_surgeons-backend-mod/README.md
+++
@@ -1,1 +0,0 @@
-# society_of_surgeons
--- society_of_surgeons-backend-mod/server/routes/api/adminRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')();
-
-
-router.prefix('/admin');
-//todo: add admin controller to the admin route
-
-
-module.exports = router;
