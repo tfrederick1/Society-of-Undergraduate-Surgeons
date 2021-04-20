4629af7e57f2af4beae1ebc607bee3881692b6f4
chasonpratt <58475164+chasonpratt@users.noreply.github.com>
2021-04-19 21:19:45 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
   app.use(
-    proxy(["/api/*"], { target: "http://localhost:5000" })
+    createProxyMiddleware(["/api/*"], { target: "http://localhost:5000" })
   );
 };
