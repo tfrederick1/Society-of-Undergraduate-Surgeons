a0bac5a82cfed83a20606e0b9e267a074a3728c5
chasonpratt <58475164+chasonpratt@users.noreply.github.com>
2021-04-19 23:16:41 -0400

    Add files via upload

--- server/routes/api/userRoute.js
+++ server/routes/api/userRoute.js
@@ -1,13 +1,10 @@
 const router = require('koa-router')();
+const { postRegister } = require('../../controller/user/index');
 const controller = require('../../controller/user/index');
 
 
 router.prefix('/user');
-router.post('/register', controller.postRegister);
+router.post('/register', postRegister);
 router.post('/addMember', controller.postMember);
-router.post('/login', controller.postLogin);
-router.post('/updatePage', controller.updateContent);
-router.post('/updateMember', controller.updateMember);
-router.post('/addContent', controller.postContent);
-router.post('/updateTest', controller.postTest);
+//router.post("/addEvent", controller.postEvent);
 module.exports = router;
